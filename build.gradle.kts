6f94a1796d86a32ba4425f5b87c10d5834908ac3
j0ta3m3 <jmsanchezj@iesrafaelalberti.es>
2022-01-24 16:08:52 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    application
+
+    id("org.jetbrains.dokka") version "1.6.10"
+}
+
+repositories {
+
+
+    mavenCentral()
+
+}
+
+
+
+group = "me.jmsj"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
